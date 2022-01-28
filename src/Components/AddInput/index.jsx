7b30ec6b3abe48f3b9b02ff572ec8082b085e35a
igorasr <igoraugustosouza00@gmail.com>
2022-01-28 16:42:16 -0300

    Firts commit

--- src/Components/AddInput/index.jsx
+++ src/Components/AddInput/index.jsx
@@ -9,6 +9,12 @@ const AddInput = () => {
 
   const { handleSetTask } = useContext(TaskContext);
 
+  function handleKeyUp(e) {
+    if (e.code === "Enter" && inputData !== "") {
+      handleAddTaskClick();
+    }
+  }
+
   // Seta o state do input toda vez que ele é alterado
   function handleInputChange(e) {
     setInputData(e.target.value);
@@ -16,8 +22,12 @@ const AddInput = () => {
 
   // Funcao para adicionar as tarefas ao click do usuario
   function handleAddTaskClick() {
-    handleSetTask(inputData);
-    setInputData("");
+    if (inputData !== "" || inputData === null) {
+      handleSetTask(inputData);
+      setInputData("");
+    } else {
+      alert("Informe uma tarefa");
+    }
   }
 
   return (
@@ -25,6 +35,7 @@ const AddInput = () => {
       <div className="addinput-container">
         <input
           onChange={handleInputChange}
+          onKeyUp={handleKeyUp}
           value={inputData}
           type="text"
           placeholder="Adcione uma tarefa..."
